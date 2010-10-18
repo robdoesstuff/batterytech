b02bea7f5addb8c01c3758450cc7b6ad53f68d7f
Rob Green <rgreen@bitpowerstudios.com>
2010-10-18 04:09:41 +0000

    batterytech ios import redo

---
+++ src/batterytech-ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  batterytech-ios
+//
+//  Created by Apple on 10/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
