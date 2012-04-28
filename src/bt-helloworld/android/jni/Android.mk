5e73ab998bd846ca18d12fbe4128eb61dcec77f3
Rob Green <rgreen@bitpowerstudios.com>
2012-04-28 23:13:18 +0000

    BT 2.0 - Androd helloworld working

--- src/bt-helloworld/android/jni/Android.mk
+++ src/bt-helloworld/android/jni/Android.mk
@@ -1,6 +1,8 @@
 # Batterytech Demo App Android makefile
 
-LOCAL_PATH := $(call my-dir)/../../batterytech/src
+include $(CLEAR_VARS)
+
+LOCAL_PATH := ../batterytech/src
 
 # List all of your local source files here that you want included in this build
 my_src_files :=\
@@ -36,9 +38,9 @@ my_src_files :=\
 	batterytech/render/GLAssimpBinding.cpp \
 	batterytech/render/GLAssimpMeshBinding.cpp \
 	batterytech/render/Texture.cpp \
-	batterytech/platform/win32/win32general.cpp \
-	batterytech/platform/win32/boot.cpp \
-	batterytech/platform/win32/WinSound.cpp \
+	batterytech/platform/android/androidgeneral.cpp \
+	batterytech/platform/android/boot.cpp \
+	batterytech/platform/android/importgl.cpp \
 	batterytech/platform/opengles.cpp \
 	batterytech/platform/platformgeneral.cpp \
 	batterytech/ui/Button.cpp \
@@ -62,18 +64,17 @@ my_src_files :=\
 	batterytech/util/PropertiesIO.cpp \
 	batterytech/util/strx.cpp
 
-include $(CLEAR_VARS)
-
 LOCAL_CFLAGS := -DANDROID_NDK
-
 LOCAL_MODULE    := batterytech
-LOCAL_SRC_FILES := $(my_src_files)
-	
+LOCAL_SRC_FILES := $(my_src_files)	
 LOCAL_LDLIBS := -ldl -llog
 
 include $(BUILD_STATIC_LIBRARY)
 
 # Now for the app-specific portion
+
+include $(CLEAR_VARS)
+
 LOCAL_PATH := ../bt-helloworld/app-src
 
 my_src_files :=\
@@ -81,16 +82,13 @@ my_src_files :=\
 	HelloWorldApp.cpp \
 	RenderUtil.cpp
 
-include $(CLEAR_VARS)
-
+LOCAL_MODULE    := batterytech-app
 LOCAL_CFLAGS := -DANDROID_NDK \
                 -I ../batterytech/src
-
-LOCAL_MODULE    := batterytech-app
 LOCAL_SRC_FILES := $(my_src_files)
-	
 LOCAL_LDLIBS := -ldl -llog
-LOCAL_WHOLE_STATIC_LIBRARIES := batterytech
+LOCAL_STATIC_LIBRARIES := batterytech
+# LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
 
 include $(BUILD_SHARED_LIBRARY)
 
