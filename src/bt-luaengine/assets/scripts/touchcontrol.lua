016253f1d892e4bfedf527ec26a7c4637da575f7
Rob Green <rgreen@bitpowerstudios.com>
2012-05-07 06:25:15 +0000

    lua engine - added touch control

---
+++ src/bt-luaengine/assets/scripts/touchcontrol.lua
@@ -0,0 +1,14 @@
+
+TouchControl = {}
+
+function TouchControl.new()
+	local self = table.copy(TouchControl)
+	return self
+end
+
+function TouchControl:update(delta)
+end
+
+function TouchControl:render()
+end
+
