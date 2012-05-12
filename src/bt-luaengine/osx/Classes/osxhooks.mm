eaa5a0cfd4af3fdc06c0dbdd7f71e2e829bb2817
Rob Green <rgreen@bitpowerstudios.com>
2012-05-12 02:46:36 +0000

    BT2 osx

---
+++ src/bt-luaengine/osx/Classes/osxhooks.mm
@@ -0,0 +1,14 @@
+#include <batterytech/primitives.h>
+
+void _platform_show_ad() {
+	// Call out to your ios ad integration piece here
+}
+
+void _platform_hide_ad() {
+	// Call out to your ios ad integration piece here
+}
+
+void _platform_hook(const char *hook, char *result, S32 resultLen) {
+	// Handle custom hooks here
+}
+
