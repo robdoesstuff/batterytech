7df43abc976d471c1cb2a8bcec3b5af259e15277
Rob Green <rgreen@bitpowerstudios.com>
2012-05-14 04:52:08 +0000

    BT2 - trying to get ios stuff working

---
+++ src/bt-box2ddemo/ios/Classes/main.m
@@ -0,0 +1,25 @@
+/*
+ * BatteryTech
+ * Copyright (c) 2010 Battery Powered Games, LLC.
+ *
+ * This code is a component of BatteryTech and is subject to the 'BatteryTech
+ * End User License Agreement'.  Among other important provisions, this
+ * license prohibits the distribution of source code to anyone other than
+ * authorized parties.  If you have any questions or would like an additional
+ * copy of the license, please contact: support@batterypoweredgames.com
+ */
+
+//============================================================================
+// Name        : main.m
+// Description : IOS App Startup
+//============================================================================
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
