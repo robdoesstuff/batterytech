4827e3e1fe1fd82505f2b3877ff9407ba1781021
Rob Green <rgreen@bitpowerstudios.com>
2012-06-30 17:28:11 +0000

    BT2 - added asset override for lua button

--- src/bt-luaengine/assets/scripts/uifactory.lua
+++ src/bt-luaengine/assets/scripts/uifactory.lua
@@ -9,12 +9,20 @@ function makeButton(left, top, width,height, label)
 	return button
 end
 
-function makeButtonCentered(x,y, width,height, label)
+function makeButtonCentered(x,y, width,height, label, normalAsset, pressedAsset)
 	local sX, sY = scaleXY(x,y)
 	local sWidth, sHeight = scaleForUI(width,height)
 	local button = UIButton.new(sX-sWidth/2, sY-sHeight/2, sWidth, sHeight)
-	button.normalAsset = "ui/button1_tex.png"
-	button.pressedAsset = "ui/button1_pressed_tex.png"
+	if not normalAsset then
+		button.normalAsset = "ui/button1_tex.png"
+	else
+		button.normalAsset = normalAsset
+	end
+	if not pressedAsset then
+		button.pressedAsset = "ui/button1_pressed_tex.png"
+	else
+		button.pressedAsset = pressedAsset
+	end
 	button.label = label
 	return button
 end
