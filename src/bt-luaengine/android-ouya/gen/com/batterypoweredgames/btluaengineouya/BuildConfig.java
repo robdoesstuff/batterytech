61051ea1bd3ae2d7a41080ca34bcf94e022b693a
Rob Green <rgreen@bitpowerstudios.com>
2013-01-30 00:24:45 +0000

    BT - Android project updates

---
+++ src/bt-luaengine/android-ouya/gen/com/batterypoweredgames/btluaengineouya/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.batterypoweredgames.btluaengineouya;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
